2a74bbcb2f0c9d61f4416304c7765aa9ebca6ea4
Reinan Gabriel <reinangabriel1520@gmail.com>
2022-07-31 16:06:20 -0300

    Update install-packages.sh

--- src/os/linux/install-packages.sh
+++ src/os/linux/install-packages.sh
@@ -33,6 +33,7 @@ HASH=`curl -sS https://composer.github.io/installer.sig`
 echo $HASH
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
 composer --version
 
 # Install oh-my-zsh
