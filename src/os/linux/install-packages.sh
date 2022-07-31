6d6da5c543333a570b40de14a488e1ff48ca19a5
Reinan Gabriel <reinangabriel1520@gmail.com>
2022-07-31 16:04:25 -0300

    Update install-packages.sh

--- src/os/linux/install-packages.sh
+++ src/os/linux/install-packages.sh
@@ -13,6 +13,7 @@ declare -a packages=(
     "python3-neovim"
 )
 
+cd ~
 rm $log_file
 sudo apt update
 
@@ -25,3 +26,14 @@ do
         echo "[x] $package failed to installed" >> $log_file
     fi
 done
+
+# Install composer
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer --version
+
+# Install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
