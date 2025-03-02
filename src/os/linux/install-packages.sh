55fa3550d46a8b04ca9c872907c1cbdc62a2aca2
Reinan Gabriel <reinangabriel1520@gmail.com>
2025-03-01 22:28:18 -0300

    Update install-packages.sh

--- src/os/linux/install-packages.sh
+++ src/os/linux/install-packages.sh
@@ -5,28 +5,16 @@ log_file=~/install_dotfiles.log
 declare -a packages=(
     "zsh" 
     "zsh-syntax-highlighting"
-    "php-cli"
     "unzip"
     "build-essential"
     "make"
     "neovim"
     "python3-neovim"
-    "terraform"
+    "ripgrep"
 )
 
 cd ~
 rm $log_file
-
-# Install terraform
-# Doc: https://www.terraform.io/cli/install/apt
-
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update
 
@@ -40,14 +28,24 @@ do
     fi
 done
 
-# Install composer
-curl -sS https://getcomposer.org/installer -o composer-setup.php
-HASH=`curl -sS https://composer.github.io/installer.sig`
-echo $HASH
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-rm composer-setup.php
-composer --version
+# Astronvim
+
+## Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm -rf lazygit.tar.gz
+rm -rf lazygit
+
+## Gdu
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
+chmod +x gdu_linux_amd64
+sudo mv gdu_linux_amd64 /usr/bin/gdu
+
+## Bottom
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.10.2/bottom_0.10.2-1_amd64.deb
+sudo dpkg -i bottom_0.10.2-1_amd64.deb
 
 # Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
